46acd81a3fa99fc41c2979ffb9c176baf168adbb
Niki Plushie <sugarplushie@protonmail.com>
2021-06-13 21:21:43 -0300

    [⚙️] removed tiling layout

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -92,7 +92,7 @@ awful.util.terminal = terminal
 awful.util.tagnames = { "💾", "📒", "📔", "📄", "🎲" }
 awful.layout.layouts = {
     awful.layout.suit.floating,
-    awful.layout.suit.tile,
+    --awful.layout.suit.tile,
     --awful.layout.suit.tile.left,
     --awful.layout.suit.tile.bottom,
     --awful.layout.suit.tile.top,
